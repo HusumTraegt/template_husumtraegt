c706825dbae7d976924978c7468d454ccca310a2
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-06-30 22:36:42 +0200

    Add Fluid template

--- Configuration/TypoScript/page/page_setup.ts
+++ Configuration/TypoScript/page/page_setup.ts
@@ -1,5 +1,9 @@
 page = PAGE
 page {
-	10 = TEXT
-	10.value = Es geht bald los.
+	10 = FLUIDTEMPLATE
+	10 {
+		layoutRootPath = EXT:template_husumtraegt/Resources/Private/Page/Layouts/
+		partialRootPath = EXT:template_husumtraegt/Resources/Private/Page/Partials/
+		file = EXT:template_husumtraegt/Resources/Private/Page/Templates/Main.html
+	}
 }
