c92a2bf68004559870ce28595ba46cc0466f9acb
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-06-30 22:16:12 +0200

    Fill ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,3 +1,26 @@
 <?php
 
+/**************************************************************************
+ * Extension Manager/Repository config file for ext "template_husumtraegt".
+ *************************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Template "Husum trägt"',
+	'description' => 'TYPO3 Template Extension for http://husum-traegt.de',
+	'category' => 'frontend',
+	'author' => 'Sebastian Michaelsen',
+	'author_email' => 'sebastian.michaelsen@t3seo.de',
+	'author_company' => 't3seo.de',
+	'state' => 'stable',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'typo3' => '6.1-0.0.0',
+		),
+	),
+);
+
 ?>
