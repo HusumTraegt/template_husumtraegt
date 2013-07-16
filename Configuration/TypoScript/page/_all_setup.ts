864bd9e4a897c709283e4bf04ad8b6a5b91a735e
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-07-16 22:48:30 +0200

    Copy stuff from the design dummy

--- Configuration/TypoScript/page/_all_setup.ts
+++ Configuration/TypoScript/page/_all_setup.ts
@@ -1,1 +1,2 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:template_husumtraegt/Configuration/TypoScript/page/page_setup.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template_husumtraegt/Configuration/TypoScript/page/assets_setup.ts">
---
+++ Configuration/TypoScript/page/assets_setup.ts
@@ -0,0 +1,8 @@
+page {
+	includeCSS {
+		normalize = EXT:template_husumtraegt/Resources/Public/Stylesheets/normalize.css
+		main = EXT:template_husumtraegt/Resources/Public/Stylesheets/styles.css
+		fontOpenSans = http://fonts.googleapis.com/css?family=Open+Sans:400
+		fontOpenSans.external = 1
+	}
+}
