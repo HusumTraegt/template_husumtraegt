4ccabf2a5ee724bf90159582356aa3698b38a854
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2014-01-18 23:30:45 +0100

    Imagery for alternative design

--- Configuration/TypoScript/lib/keyvisual_setup.ts
+++ Configuration/TypoScript/lib/keyvisual_setup.ts
@@ -1,7 +1,12 @@
-[globalVar = TSFE:id = 3]
-lib.keyvisual = TEXT
-lib.keyvisual{
+temp.keyvisual = TEXT
+temp.keyvisual {
 	value = ./typo3conf/ext/template_husumtraegt/Resources/Public/Images/keyvisual.jpg
 	wrap = <img class="mood" src="|">
 }
+
+[globalVar = TSFE:id = 3]
+lib.keyvisual < temp.keyvisual
+[globalVar = TSFE:id = 13]
+lib.keyvisual < temp.keyvisual
+lib.keyvisual.value = ./typo3conf/ext/template_husumtraegt/Resources/Public/Images/keyvisual_diapers.jpg
 [global]
